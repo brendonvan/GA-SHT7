30a98633f6324a6cfe4ebcfebe84a8b07dde94f0
hpweisberg <hpweisberg@gmail.com>
2023-03-13 13:30:03 -0700

    create setup of form, create handleSubmit and handleChange

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -1,10 +1,53 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
 const Onboarding = () => {
+  const [formData, setFormData] = useState({
+    //? Awaiting backend model
+  })
+
+  const navigate = useNavigate()
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async evt => {
+    evt.preventDefault()
+    try {
+      //? budgetService not created yet
+      await budgetService.create(formData)
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
-  
-  return ( 
+  return (
     <>
-      
+      <form
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
+        <div>
+          <label htmlFor="XXX">XXX</label>
+          <input
+            type='text'
+            autoComplete="off"
+            value={formData.XXX}
+            onChange={handleChange}
+            name='XXX'
+          >
+          </input>
+        </div>
+      <div>
+        <button>
+          Submit
+        </button>
+      </div>
+      </form>
     </>
   )
 }
