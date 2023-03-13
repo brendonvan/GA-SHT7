b6b831dcaa68931b943c7af690d7a81e7d2228e5
brendonvan <brendon@van.digital>
2023-03-13 13:39:03 -0700

    setup redux

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,31 @@
 import styles from './Landing.module.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { incAction, decAction } from '../../actions'
 
 const Landing = ({ user }) => {
+  const preview = useSelector(state => state.counterReducer);
+  const dispatch = useDispatch();
+
+  function handleIncrement() {
+    dispatch(incAction());
+  }
+
+  function handleDecrement() {
+    dispatch(decAction());
+    
+  }
+
   return (
     <main className={styles.container}>
       <h1>hello, {user ? user.name : 'friend'}</h1>
+      <div>
+        <h2>Counter</h2>
+        <div>
+          <button onClick={() => {handleDecrement()}}>-</button>
+          <span>{preview}</span>
+          <button onClick={() => {handleIncrement()}}>+</button>
+        </div>
+      </div>
     </main>
   )
 }
